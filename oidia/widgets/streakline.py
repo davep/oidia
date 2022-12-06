048c8a502131000b739429034f536bc22d5bb7f0
Dave Pearson <davep@davep.org>
2022-12-06 13:57:13 +0000

    :art: Fix wonky indentation

--- oidia/widgets/streakline.py
+++ oidia/widgets/streakline.py
@@ -56,14 +56,14 @@ class StreakLine( Timeline ):
    """Widget to display a horizontal timeline of streak results."""
 
    def make_my_day( self, day: date ) -> StreakDay:
-        """Make a day widget for the given day.
+       """Make a day widget for the given day.
 
-        Args:
-            day (date): The date to make the day widget for.
+       Args:
+           day (date): The date to make the day widget for.
 
-        Returns:
-            StreakDay: The day widget for the timeline.
-        """
-        return StreakDay( day )
+       Returns:
+           StreakDay: The day widget for the timeline.
+       """
+       return StreakDay( day )
 
 ### streakline.py ends here
