cb8d81c5b46b772ddcea8c6d1b01a353d239c744
Dave Pearson <davep@davep.org>
2023-01-21 11:12:19 +0000

    :arrow_up: Upgrade to Textual 0.10.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     packages                      = find_packages(),
     package_data                  = { "oidia": [ "py.typed" ] },
     include_package_data          = True,
-    install_requires              = [ "textual==0.9.1", "xdg" ],
+    install_requires              = [ "textual==0.10.1", "xdg" ],
     python_requires               = ">=3.10",
     keywords                      = "terminal textual streak todo",
     entry_points                  = {
